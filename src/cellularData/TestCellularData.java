9a443b2634b9f9490b5bacb6f8b6ba07af172243
William Tsai <wtsai_us@yahoo.com>
2018-04-17 22:23:58 -0700

    started creating CellularData class

--- src/cellularData/TestCellularData.java
+++ src/cellularData/TestCellularData.java
@@ -5,7 +5,7 @@ package cellularData;
  *  Given a country name and a range of years, computes the total number of subscriptions
  *  during that period.
  *
- *  @author Foothill College, Bita M, [YOUR NAME HERE]
+ *  @author William Tsai
  */
 public class TestCellularData 
 {
